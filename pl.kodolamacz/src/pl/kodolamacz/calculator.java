38dcf0cef90341b5504e4aa38fa997a5e02d58fe
bjei4o <bjei4o@gmail.com>
2018-04-26 11:30:56 +0200

    First commin 1.1

--- pl.kodolamacz/src/pl/kodolamacz/calculator.java
+++ pl.kodolamacz/src/pl/kodolamacz/calculator.java
@@ -1,4 +1,10 @@
 package pl.kodolamacz;
+/**
+ * 
+ * @author bev
+ * Simple console calculator which asking for first number, next asking for type of action
+ * and after that asking for second number. Then it gives result.
+ */
 
 public class calculator {
 
