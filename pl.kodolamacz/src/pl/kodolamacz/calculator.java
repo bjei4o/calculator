498f15660cc42add74b85b36c7d788c06d908250
bjei4o <bjei4o@gmail.com>
2018-04-26 19:01:52 +0200

    Added method which calculating equation

--- pl.kodolamacz/src/pl/kodolamacz/calculator.java
+++ pl.kodolamacz/src/pl/kodolamacz/calculator.java
@@ -1,4 +1,7 @@
 package pl.kodolamacz;
+
+import java.util.Scanner;
+
 /**
  * 
  * @author bev
@@ -9,8 +12,56 @@ package pl.kodolamacz;
 public class calculator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner loadData = new Scanner(System.in);
+		System.out.println("Welcome in calculator app!");
+	
+			
+		
+		System.out.println("What kind of operation do you want to make? Addition, Substract, Divide, Multiply or Modulo");
+		System.out.println("(write +, -, /, * or % and confirm by enter) or \"q\" to exit");
+			
+		String typeOfAction = loadData.next();
 
+		System.out.println("Enter first number: ");
+		Float number1 = loadData.nextFloat();
+		System.out.println("Enter second number: ");
+		Float number2 = loadData.nextFloat();
+			
+		System.out.print(result(typeOfAction, number1, number2));
+		
+		loadData.close();
+	}
+	
+	/**
+	 * 
+	 * @param act - sending type of action
+	 * @param a - first number
+	 * @param b - second number
+	 * @return result - result of operation between number1 and number2
+	 */
+	public static String result (String act, float a, float b) {
+		String result = "";
+		String operation = a+" "+act+" "+b+" = ";
+		
+		if(act.equals("+"))
+			result = operation+Float.toString(a+b);
+		else if(act.equals("-"))
+			result = operation+Float.toString(a-b);
+		else if(act.equals("/")) {
+			if(b == 0)
+				result = "Can't divide by 0";
+			else
+				result = operation+Float.toString(a/b);
+		}
+		else if(act.equals("*"))
+			result = operation+Float.toString(a*b);
+		else if(act.equals("%"))
+			result = operation+Float.toString(a%b);
+		else
+			result = "There is no such operation!";
+		
+		return result;
 	}
 
 }
